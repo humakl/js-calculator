d2b35e627802f1260ba6fdcac0e574dbbddc085a
humakl <st.sinnerman@gmail.com>
2021-03-22 03:43:57 +0400

    some more preparations

--- js/script.js
+++ js/script.js
@@ -15,17 +15,25 @@ let multiplication = document.getElementById('multiplication');
 let division = document.getElementById('division');
 let result = document.getElementById('result');
 let clear = document.getElementById('clear');
-let pole = document.getElementById('window');
+let pole = document.getElementById('screen');
+let number = document.getElementsByClassName('number');
 
+//0. Начальное состояние строки ввода (в дальнейшем "экран") "0"
+//1. При нажатии на цифры и точку - добавляется на экран
+//2. Резльтат запоминается, пока не сотрем с помощью "С"
+//3. При нажатии на оператор инпут очищается, операнд запоминается
+//   При очередном нажатии на оператор, прозиводится предыдущая операция и запоминатся для дальнейших дествий
+//4. При нажатии на "=" выводится результат на экран
+//5. При нажатии на "С" экран переходит в начальное состояние и память очищается
 
-let btn = (btn) => document.getElementById(btn).innerHTML;
+function onButtonClick(eventObject) {
+  let clickedElement = eventObject.currentTarget;
+  let operation = clickedElement.innerHTML;
+  console.log(operation);
+}
 
-let alertt = () => alert('one');
+let ton = document.getElementsByTagName('button');
 
-let putInWindow = () => pole.value = 4;
-
-one.addEventListener('click', alertt);
-two.addEventListener('click', alertt);
-three.addEventListener('click', alertt);
-four.addEventListener('click', putInWindow);
-clear.addEventListener('click', putInWindow);
+for (i = 0; i < ton.length; i++) {
+  ton[i].addEventListener('click', onButtonClick)
+}
